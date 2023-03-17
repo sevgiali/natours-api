028499308af9c1476fc5ead832b6b4e0dc975efd
sevgiali <sevgiialii97@gmail.com>
2023-03-17 15:50:42 +0200

    handling get requests

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello form the server side!', app: 'Natours' });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'succes',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
-app.post('/', (req, res) => {
-  res.send('You can post to this  endpoint...');
-});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port} ...`);
