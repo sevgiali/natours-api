adc2e60562d91ef3daa99638d623770a4e474913
sevgiali <sevgiialii97@gmail.com>
2023-03-20 16:05:22 +0200

    setting up ESlint and Prettier

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -8,7 +8,7 @@ exports.checkID = (req, res, next, val) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
-      message: 'Invalid ID',
+      message: 'Invalid ID'
     });
   }
   next();
@@ -18,7 +18,7 @@ exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
     return res.status(400).json({
       status: 'fail',
-      message: 'Missing name or price',
+      message: 'Missing name or price'
     });
   }
   next();
@@ -30,20 +30,20 @@ exports.getAllTours = (req, res) => {
     requestedAt: req.requestTime,
     results: tours.length,
     data: {
-      tours: tours,
-    },
+      tours: tours
+    }
   });
 };
 
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
+  const tour = tours.find(el => el.id === id);
 
   res.status(200).json({
     status: 'succes',
     data: {
-      tour,
-    },
+      tour
+    }
   });
 };
 
@@ -54,12 +54,12 @@ exports.createTour = (req, res) => {
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
-    (err) => {
+    err => {
       res.status(201).json({
         status: 'succes',
         data: {
-          tour: newTour,
-        },
+          tour: newTour
+        }
       });
     }
   );
@@ -69,14 +69,14 @@ exports.updateTour = (req, res) => {
   res.status(200).json({
     status: 'success',
     data: {
-      tour: '<Updated tour here...>',
-    },
+      tour: '<Updated tour here...>'
+    }
   });
 };
 
 exports.deleteTour = (req, res) => {
   res.status(204).json({
     status: 'success',
-    data: null,
+    data: null
   });
 };
