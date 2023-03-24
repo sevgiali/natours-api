932f388dc5986cc4c46e5c608d5a55cc0c3f26db
sevgiali <sevgiialii97@gmail.com>
2023-03-24 17:28:03 +0200

    document middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,8 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true
     },
+
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration']
@@ -63,6 +66,12 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
+//document middleware
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
