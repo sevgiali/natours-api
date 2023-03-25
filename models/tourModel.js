ff4e133db80e2318ce5e6066fe779f82b1bbc6a2
sevgiali <sevgiialii97@gmail.com>
2023-03-25 17:21:25 +0200

    Query middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -55,7 +55,11 @@ const tourSchema = new mongoose.Schema(
       default: Date.now(),
       select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false
+    }
   },
   {
     toJSON: { virtuals: true },
@@ -72,6 +76,19 @@ tourSchema.pre('save', function(next) {
   next();
 });
 
+///query middleware
+tourSchema.pre(/^find/, function(next) {
+  // tourSchema.pre('find', function(next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+  console.log(docs);
+  next();
+});
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
