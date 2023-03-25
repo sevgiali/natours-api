12410a35e1e3c8aea6985f01e072d957150ad9f8
sevgiali <sevgiialii97@gmail.com>
2023-03-25 17:55:10 +0200

    Data Validation: Built-In Validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -7,7 +7,12 @@ const tourSchema = new mongoose.Schema(
       type: String,
       required: [true, 'A tour must have a name'],
       unique: true,
-      trim: true
+      trim: true,
+      maxLength: [
+        40,
+        ' A tour name must have less or equal then 40 characters'
+      ],
+      minLength: [10, ' A tour name must have more or equal then 40 characters']
     },
 
     slug: String,
@@ -21,11 +26,17 @@ const tourSchema = new mongoose.Schema(
     },
     difficulty: {
       type: String,
-      required: [true, 'A tour must have a difficulty']
+      required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either : easy,medium or difficult'
+      }
     },
     ratingsAverage: {
       type: Number,
-      default: 4.5
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0']
     },
     ratingsQuantity: {
       type: Number,
